c9308ff23b0c80234ce9102ef9fcdc8aff231382
gabriel-caetano <gvcaetano@inf.ufsm.br>
2021-03-02 21:29:34 -0300

    fix: removing useless methods from system call and fixing job1 instruction intentionally damaged to test

--- system_call.py
+++ system_call.py
@@ -1,32 +1,6 @@
 from cpu import Cpu
 
 class SystemCall:
-	def __init__(self):
-		self.__syscall = { "PARA": self.__stop, "LE": self.__read, "GRAVA": self.__write }
-
-	def __stop(self, cpu, timer, job):
-		job.__setStatus('finished')
-		cpu.setState("ilegal instruction")
-		return False
-
-	def __read(self, cpu, timer, job):
-		with open("input.txt", 'r') as _input:
-			value = _input.readline(),
-			cpu.setAcc(int(value))
-		return True
-	
-	def __write(self, cpu, timer, job):
-		with open("output.txt", 'w') as _output:
-			_output.write(str(cpu.getAcc()))
-		return True
-
-	def execute(self, instruction, cpu, timer):
-		try:
-			self.__syscall[instruction](cpu, timer)
-			return True
-		except:
-			return False
-
 	def read(self, index, job, job_index):
 		job.setPc(job.getPc() + 1)
 		input_address = f'jobs/{job.getName()}/io/{job.getIo()[index].strip()}'
